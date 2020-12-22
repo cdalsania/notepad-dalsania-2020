fc2578ecb939e8d80487ac4db83f371a58684c45
Crystal Dalsania <dalsania.crystal@gmail.com>
2020-12-21 20:09:35 -0500

    created and installed gunicorn

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from os import environ
 app = Flask(__name__)
+app.config['MONGO_URI'] = environ.get(
+    'MONGODB_URI', 'mongodb://localhost:27017/notepad')
+mongo = PyMongo(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'DATABASE_URL', 'sqlite:///notepad.sqlite')
+db = SQLAlchemy(app)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
 @app.route('/')
 def index():
     return "Hello world!"
-
+@app.route('/api/tasks-postgres')
+def getTasksPostgres():
+    tasks = db.session.query(Task)
+    data = []
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'description': task.description
+        }
+        data.append(item)
+    return jsonify(data)
+@app.route('/api/tasks-mongo')
+def getTasksMongo():
+    tasks = mongo.db.tasks.find()
+    data = []
+    for task in tasks:
+        item = {
+            '_id': str(task['_id']),
+            'description': task['description']
+        }
+        data.append(item)
+    return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True)
